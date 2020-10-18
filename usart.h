be3ad5ffe2cb52137981943b6eeb55b1c8457d1e
yadavadesh <67011517+yadavadesh@users.noreply.github.com>
2020-10-18 16:53:44 +0530

    Create usart.h

---
+++ usart.h
@@ -0,0 +1,11 @@
+#ifndef USART_H
+#define USART_H
+
+#define USART_BAUD_HIGH 0x00
+#define USART_BAUD_LOW 0x33
+#define USART_STATUS_C 0x06
+#define USART_STATUS_B 0xD8
+
+void usartInit(void) ;
+
+#endif
