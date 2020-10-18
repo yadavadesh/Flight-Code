30433ab4e12b9c2839384dfb2b66a7585800b758
yadavadesh <67011517+yadavadesh@users.noreply.github.com>
2020-10-18 16:50:30 +0530

    Update usart.c

--- usart.c
+++ usart.c
@@ -4,10 +4,10 @@
 void usartInit (void)
 {
     	/*setting baud rate to 9600*/
-	    UBRR0H=0x00;
-	    UBRR0L=0x33;
-	    /*setting no. of data bits to 8*/
-	    UCSR0C=0x06;
-	    /*enable receive complete interrupt, transmit complete interrupt, receiver enable, transmitter enable*/
-	    UCSR0B=0xD8;
+	UBRR0H=0x00;
+	UBRR0L=0x33;
+	/*setting no. of data bits to 8*/
+	UCSR0C=0x06;
+	/*enable receive complete interrupt, transmit complete interrupt, receiver enable, transmitter enable*/
+	UCSR0B=0xD8;
 }
